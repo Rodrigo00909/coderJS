84c4540ae8c7dd3718c173451387be734d53c5da
Rodrigo Torres <54510225+Rodrigo00909@users.noreply.github.com>
2021-07-28 19:10:33 -0300

    subida desafio 2 completa

---
+++ Desafio_2/scripts/main.js
@@ -0,0 +1,7 @@
+let edad = prompt("Para ingresar al bar tenes que ser mayor de 18. ¿Cuantos años tenés?");
+
+if(edad >= 18) {
+    alert("¡Bien! Sos mayor de edad, podés ingresar al bar.");
+} else {
+    alert("Todavía no sos mayor de edad, volvé en unos días! - O pedí prestado un documento ;) -");
+}
