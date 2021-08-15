4495f8b67545e988f7ba1f5e2ef649209d1fd56a
Rodrigo Torres <54510225+Rodrigo00909@users.noreply.github.com>
2021-08-14 22:57:36 -0300

    subida practica con objetos y arrays

---
+++ Others/map-filter-find.js
@@ -0,0 +1,21 @@
+
+
+// El map es similar al for, lo reemplaza. Siempre que retorna algo lo retorna en un nuevo array
+// En este caso me pinta todos los numeros X
+
+
+// A cada uno de los items lo  multiplico por dos
+
+
+// Devuelve el producto que conincida con el id 3
+
+// devuelve los precios menores a 100
+
+// devuelve los precios sumandoles 30
+
+
+// Metodo some: El método some() comprueba si al menos un elemento del array cumple con la condición implementada por la función proporcionada.
+
+
+// For each(menos usado) 
+
