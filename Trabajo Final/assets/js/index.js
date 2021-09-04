3382aad7885ddc05f64f2b488ba6c4a352cc1e58
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-09-04 01:56:27 -0300

    funcionalidades y validaciones con jquery

--- Trabajo Final/assets/js/index.js
+++ Trabajo Final/assets/js/index.js
@@ -1,159 +1,185 @@
 /* Construir CV */
 const btnGenerarCv = document.querySelector(".btnGenerarCv");
 
-btnGenerarCv.addEventListener("click", () => {
-    /* Generar Perfil */
-    class Perfil {
-        constructor(n,p,t,l,g) {
-            this.nameField = n;
-            this.professionField = p;
-            this.telField = t;
-            this.linkField = l;
-            this.mailField = g;
+
+$(btnGenerarCv).click( (e) => {
+    e.preventDefault();
+
+    const nombre = $.trim($("#nameField").val());
+    const profesion = $.trim($("#professionField").val());
+    const tel = $.trim($("#telField").val());
+    const email = $.trim($("#mailField").val());
+    const objetivo = $.trim($("#objField").val());
+
+    const btnComprobar = $("#agregarAlert");
+
+    if(nombre == '' || profesion == '' || tel == '' || email == '' || objetivo == ''){
+        /* console.log('Hay campos sin completar');
+        return false; */
+        $(document).ready(function() {
+            $(btnComprobar).html("Porfavor, llenar los campos obligatorios")
+        })
+    } else {
+    
+        /* Generar Perfil */
+        class Perfil {
+            constructor(n,p,t,l,g) {
+                this.nameField = n;
+                this.professionField = p;
+                this.telField = t;
+                this.linkField = l;
+                this.mailField = g;
+            }
         }
-    }
     
     
-    const nameField = document.querySelector("#nameField").value;
-    localStorage.setItem('nombre', nameField);
-    const setName = document.querySelector("#name");
+        const nameField = document.querySelector("#nameField").value;
+        localStorage.setItem('nombre', nameField);
+        const setName = document.querySelector("#name");
 
-    const professionField = document.querySelector("#professionField").value;
-    localStorage.setItem('profesion', professionField);
-    const setProf = document.querySelector("#prof");
+        const professionField = document.querySelector("#professionField").value;
+        localStorage.setItem('profesion', professionField);
+        const setProf = document.querySelector("#prof");
 
-    const telField = document.querySelector("#telField").value;
-    localStorage.setItem('telefono', telField);
-    const setTel = document.querySelector("#tel");
+        const telField = document.querySelector("#telField").value;
+        localStorage.setItem('telefono', telField);
+        const setTel = document.querySelector("#tel");
 
-    const linkField = document.querySelector("#linkField").value;
-    localStorage.setItem('linkedin', linkField);
-    const setLinkedin = document.querySelector("#linkedin");
+        const linkField = document.querySelector("#linkField").value;
+        localStorage.setItem('linkedin', linkField);
+        const setLinkedin = document.querySelector("#linkedin");
 
-    const mailField = document.querySelector("#mailField").value;
-    localStorage.setItem('mail', mailField);
-    const setEmail = document.querySelector("#mail");
+        const mailField = document.querySelector("#mailField").value;
+        localStorage.setItem('mail', mailField);
+        const setEmail = document.querySelector("#mail");
 
-    
-    const perfil = new Perfil(nameField, professionField, telField, linkField, mailField);
+        
+        const perfil = new Perfil(nameField, professionField, telField, linkField, mailField);
 
-    
-    // Pintar en HTML
-    setName.innerHTML = `${perfil.nameField}`;
-    setProf.innerHTML = `${perfil.professionField}`;
-    setTel.innerHTML = `${perfil.telField}`;
-    setLinkedin.innerHTML = `<a href="https://www.linkedin.com/in/${perfil.linkField}" target="_blank">${perfil.linkField}</a>`; // Agregarlo en el constructor no aca
-    setEmail.innerHTML = `${perfil.mailField}`;
-    /* /Generar Perfil */
+        
+        // Pintar en HTML
+        setName.innerHTML = `${perfil.nameField}`;
+        setProf.innerHTML = `${perfil.professionField}`;
+        setTel.innerHTML = `${perfil.telField}`;
+        setLinkedin.innerHTML = `<a href="https://www.linkedin.com/in/${perfil.linkField}" target="_blank">${perfil.linkField}</a>`; // Agregarlo en el constructor no aca
+        setEmail.innerHTML = `${perfil.mailField}`;
+        /* /Generar Perfil */
 
-    /* Generar PerfilProfesional */
-    // Genera el PerfilProfesional a partir de un objeto que recibe los valores de los inputs
-    class PerfilProfesional {
-        constructor(e,d,h) {
-            this.objField = e;
-            this.expField = d;
-            this.academicaField = h;
+        /* Generar PerfilProfesional */
+        // Genera el PerfilProfesional a partir de un objeto que recibe los valores de los inputs
+        class PerfilProfesional {
+            constructor(e,d,h) {
+                this.objField = e;
+                this.expField = d;
+                this.academicaField = h;
+            }
+            mensaje() {
+                console.log("Experiencia agregada con exito!");
+            }
         }
-        mensaje() {
-            console.log("Experiencia agregada con exito!");
+
+        const objField = document.querySelector("#objField").value;
+        const setObjetivo = document.querySelector("#objDiv");
+
+        const expField = document.querySelector("#expField").value;
+        const setExperiencia = document.querySelector("#expDiv");
+
+        const academicaField = document.querySelector("#academicaField").value;
+        const setEstudio = document.querySelector("#estudiosDiv")
+
+        
+        const perfilProfesional = new PerfilProfesional(objField, expField, academicaField);
+
+        
+        setObjetivo.innerHTML = `<p>${perfilProfesional.objField}</p>`;
+        setExperiencia.innerHTML = `<p>${perfilProfesional.expField}</p>`;
+        setEstudio.innerHTML = `<p>${perfilProfesional.academicaField}</p>`;
+
+        perfilProfesional.mensaje();
+        /* / Generar PerfilProfesional */
+
+        /* Generar Skills */
+        // Genera Skills a partir de un Array de objetos obteniendo valores de los inputs
+        class Skills {
+            constructor(skill) {
+                this.skill = skill;
+
+            }
+            mensaje() {
+                console.log("Skills agregados con exito");
+            }
         }
-    }
 
-    const objField = document.querySelector("#objField").value;
-    const setObjetivo = document.querySelector("#objDiv");
+        let arraySkills = [];
+        const setSkills = document.querySelector("#cv_skills");
 
-    const expField = document.querySelector("#expField").value;
-    const setExperiencia = document.querySelector("#expDiv");
+        let skill = document.querySelectorAll(".skill");
 
-    const academicaField = document.querySelector("#academicaField").value;
-    const setEstudio = document.querySelector("#estudiosDiv")
-
-    
-    const perfilProfesional = new PerfilProfesional(objField, expField, academicaField);
-
-    
-    setObjetivo.innerHTML = `<p>${perfilProfesional.objField}</p>`;
-    setExperiencia.innerHTML = `<p>${perfilProfesional.expField}</p>`;
-    setEstudio.innerHTML = `<p>${perfilProfesional.academicaField}</p>`;
-
-    perfilProfesional.mensaje();
-    /* / Generar PerfilProfesional */
-
-    /* Generar Skills */
-    // Genera Skills a partir de un Array de objetos obteniendo valores de los inputs
-    class Skills {
-        constructor(skill) {
-            this.skill = skill;
-
+        // Obtiene cada valor de los inputs con la clase skill, crea un objeto con dichos valores y luego los
+        /// asigna a un array para generarlos en el html
+        for(let i = 0; i < skill.length; i++) {
+            // arraySkills[i] = skills[i].value;
+            arraySkills = new Skills(skill[i].value);
+            //console.log(arraySkills);
+            setSkills.innerHTML += `<p>- ${arraySkills.skill}</p>`
         }
-        mensaje() {
-            console.log("Skills agregados con exito");
+
+        /* /Generar Skills */
+
+        /* Generar Tecnologías */
+
+        class Tecnologies {
+            constructor(tecnology) {
+                this.tecnology = tecnology;
+            }
         }
-    }
 
-    let arraySkills = [];
-    const setSkills = document.querySelector("#cv_skills");
+        const cvTecnologies = document.querySelector("#cv_tecnologies");
+        
+        // const arrayTecnologies = ["item 1", "item 2", "item 3"];
+        let arrayTecnologies = [];
 
-    let skill = document.querySelectorAll(".skill");
+        const fragment = document.createDocumentFragment();
+        let tecnology = document.querySelectorAll(".tecnology");
 
-    // Obtiene cada valor de los inputs con la clase skill, crea un objeto con dichos valores y luego los
-    /// asigna a un array para generarlos en el html
-    for(let i = 0; i < skill.length; i++) {
-        // arraySkills[i] = skills[i].value;
-        arraySkills = new Skills(skill[i].value);
-        //console.log(arraySkills);
-        setSkills.innerHTML += `<p>- ${arraySkills.skill}</p>`
-    }
-
-    /* /Generar Skills */
-
-    /* Generar Tecnologías */
-
-    class Tecnologies {
-        constructor(tecnology) {
-            this.tecnology = tecnology;
+        for(let i = 0; i < tecnology.length; i++) {
+            // Constructor + asignar valor de constructor al array
+            arrayTecnologies = new Tecnologies(tecnology[i].value);
+            // Crear elemento
+            const li = document.createElement("li");
+            // Asignarle cada elemento como texto al li
+            li.textContent = arrayTecnologies.tecnology;
+            // Asignar como hijo el li al fragmento para no generar reflow
+            fragment.appendChild(li);
         }
-    }
 
-    const cvTecnologies = document.querySelector("#cv_tecnologies");
-    
-    // const arrayTecnologies = ["item 1", "item 2", "item 3"];
-    let arrayTecnologies = [];
+        cvTecnologies.appendChild(fragment);
 
-    const fragment = document.createDocumentFragment();
-    let tecnology = document.querySelectorAll(".tecnology");
+        /* /Generar Tecnologías */
+        
+        /* Esconder Formulario - Mostrar CV */
+        const formulario = document.querySelector("#formulario");
+        formulario.setAttribute("class", "dnone");
 
-    for(let i = 0; i < tecnology.length; i++) {
-        // Constructor + asignar valor de constructor al array
-        arrayTecnologies = new Tecnologies(tecnology[i].value);
-        // Crear elemento
-        const li = document.createElement("li");
-        // Asignarle cada elemento como texto al li
-        li.textContent = arrayTecnologies.tecnology;
-        // Asignar como hijo el li al fragmento para no generar reflow
-        fragment.appendChild(li);
-    }
+        const curriculum = document.querySelector("#curriculum");
+        setTimeout(() => {
+            curriculum.setAttribute("class", "slide-in-right");
+            curriculum.classList.remove("dnone");
+        }, 250);
+        /* / Esconder Formulario - Mostrar CV */
 
-    cvTecnologies.appendChild(fragment);
+    }/* /Construir CV */
+});
 
-    /* /Generar Tecnologías */
-    
-    /* Esconder Formulario - Mostrar CV */
-    const formulario = document.querySelector("#formulario");
-    formulario.setAttribute("class", "dnone");
+/* btnGenerarCv.addEventListener("click", (e) => {
 
-    const curriculum = document.querySelector("#curriculum");
-    setTimeout(() => {
-        curriculum.setAttribute("class", "slide-in-right");
-        curriculum.classList.remove("dnone");
-    }, 250);
-    /* / Esconder Formulario - Mostrar CV */
-})
-/* /Construir CV */
+}) */
+
 
 /* Añadir Formación */
 const añadirFormacion = document.querySelector(".añadirFormacion");
-añadirFormacion.addEventListener("click", () => {
+añadirFormacion.addEventListener("click", (e) => {
+    e.preventDefault();
         /* Añade un nuevo campo de Formación académica junto con el botón */
     let nuevoCampo = document.createElement("textarea");
     nuevoCampo.classList.add("form-control");
@@ -171,7 +197,8 @@ añadirFormacion.addEventListener("click", () => {
 //console.log("DISTE CLICK")
 /* Añadir Experiencia */
 const añadirExp = document.querySelector(".añadirExp");
-añadirExp.addEventListener("click", () => {
+añadirExp.addEventListener("click", (e) => {
+    e.preventDefault();
     /* Añade un nuevo campo de Experiencia junto con el botón */
     
     // Crear nodo a insertar
@@ -183,15 +210,10 @@ añadirExp.addEventListener("click", () => {
     nuevoCampo.setAttribute("rows", 3);
     nuevoCampo.setAttribute("placeholder", "Tu experiencia");
 
-    /* let nuevoSpan = document.createElement("span");
-    nuevoSpan.classList.add("input-group-text"); */
-
-
     // Boton que se movera
     let añadirBtnExp = document.querySelector("#expAñadirBtn");
     // Referencia al nodo padre
     let padre = document.querySelector("#experiencia").parentNode;
-
     
     // Inserta el hijo y el boton
     padre.insertBefore(nuevoCampo,añadirBtnExp);
@@ -316,3 +338,4 @@ const delegacion = (e) => {
 
 
 })(); // (Se ejecuta cuando el navedaor se refresca parecido a onload)
+
